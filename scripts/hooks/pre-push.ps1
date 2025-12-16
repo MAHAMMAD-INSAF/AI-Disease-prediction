845221d3745c87f4c3bdbb1af90843df21be8494
MAHAMMAD-INSAF <mahammadinsaf36@gmail.com>
2025-12-16 11:25:03 +0530

    Added script to run frontend and backend concurrently

--- scripts/hooks/pre-push.ps1
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pwsh
-try {
-  & "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\..\prepush.ps1"
-  exit $LASTEXITCODE
-} catch {
-  Write-Error "Failed to run pre-push hook wrapper: $_"
-  exit 1
-}
